b400f486943c3f7cc8b985ab0d3e035e0b720cdd
Ali Modares <s.a.modares.h@gmail.com>
2018-11-13 10:53:48 +0330

    SAHelper

---
+++ securityhelper/src/main/java/ir/adp/fakehelper2/SAHelper.java
@@ -0,0 +1,45 @@
+package ir.adp.fakehelper2;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SAHelper {
+
+    public static boolean isFake(Context context, String key) {
+        String encode = convertStringToMD5(context.getPackageName());
+        if (!encode.trim().equals(key)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isEmulator() {
+        return Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT);
+    }
+
+    private static String convertStringToMD5(String s) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
